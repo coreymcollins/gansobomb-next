d7d924612a28e107734d128c8ebcb1560364e292
Corey Collins <coreymcollins@gmail.com>
2023-07-30 18:03:26 -0400

    reverts and adds props back in

--- app/tag/[slug]/page.tsx
+++ app/tag/[slug]/page.tsx
@@ -3,9 +3,9 @@ import PageFooter from '@/components/PageFooter';
 import PageHeader from '@/components/PageHeader';
 import PostPreview from '@/components/PostPreview';
 
-const TagArchive = () => {
+const TagArchive = ( props: any ) => {
     
-    const thesePosts = getAllPostsByTag( 'aew' );
+    const thesePosts = getAllPostsByTag( props.params.slug );
     
     const postPreviews = thesePosts.map( ( post ) => (
         <PostPreview key={post.slug} {...post} />
@@ -15,7 +15,7 @@ const TagArchive = () => {
         <main>
             <PageHeader />
             <div className="page-interior">
-                <h2>Posts tagged with</h2>
+                <h2>Posts tagged with {props.params.slug}</h2>
             </div>
             <div className="post-grid">
                 {postPreviews}
