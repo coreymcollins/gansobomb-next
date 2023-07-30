de9e9121e809946b86e1886f23c09fcfdb4fe77c
Corey Collins <coreymcollins@gmail.com>
2023-07-30 17:33:10 -0400

    updates single post to display tags

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import Markdown from 'markdown-to-jsx';
 import matter from 'gray-matter';
 import getPostsMetadata from '@/components/getPostMetaData';
 import Image from 'next/image';
@@ -8,6 +7,7 @@ import MarkdownRenderer from '@/components/MarkdownRenderer';
 import type { Metadata } from 'next';
 import PageHeader from '@/components/PageHeader';
 import PageFooter from '@/components/PageFooter';
+import Link from 'next/link';
 
 const getPostContent = ( slug: string ) => {
     const folder = 'posts/';
@@ -84,7 +84,16 @@ const postSingle = ( props: any ) => {
                 </div>
                 <div className="post-single-content">
                     <MarkdownRenderer content={post.content} />
+                    
+                    <div className="tag-list">
+                        { post.data.tags.map( (tag: any) => (
+                            <Link key={tag} href={`/tag/${tag}`}>
+                                {tag}
+                            </Link>
+                        ))}
+                    </div>
                 </div>
+
             </article>
             <PageFooter />
         </main>
