a8c96feb98a3c821ebb0eed161e2839df9354e25
Corey Collins <coreymcollins@gmail.com>
2023-07-30 17:32:54 -0400

    adds a slug page

---
+++ app/tag/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { getAllPostsByTag } from '@/components/getAllPostsByTag';
+import PageFooter from '@/components/PageFooter';
+import PageHeader from '@/components/PageHeader';
+import PostPreview from '@/components/PostPreview';
+
+export default function TagArchive( props: any ) {
+    
+    const thesePosts = getAllPostsByTag( props.params.slug );
+    
+    const postPreviews = thesePosts.map( ( post ) => (
+        <PostPreview key={post.slug} {...post} />
+    ));
+
+    return (
+        <main>
+            <PageHeader />
+            <div className="page-interior">
+                <h2>Posts tagged with {props.params.slug}</h2>
+            </div>
+            <div className="post-grid">
+                {postPreviews}
+            </div>
+            <PageFooter />
+        </main>
+    );
+};
