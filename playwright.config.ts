8a7194ed8187e1b578dc04551ab28381d77475be
Corey Collins <coreymcollins@gmail.com>
2023-08-11 15:17:41 -0400

    updates reuseExistingServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,6 +38,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run start',
     url: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true,
   },
 });
