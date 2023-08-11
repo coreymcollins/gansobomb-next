c221619e94fb20a2dc39502f2971f6dce3a91d64
Corey Collins <coreymcollins@gmail.com>
2023-08-11 15:23:36 -0400

    okay, updates versions and dirs again

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'e2e',
-  outputDir: 'e2e/playwright-report',
 
   // Run all tests in parallel.
   fullyParallel: true,
