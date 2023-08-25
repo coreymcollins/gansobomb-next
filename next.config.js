0ec795a5de0363b8c275ae76fb012cf76f24a7c7
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2023-08-25 18:36:27 -0400

    adds a few lingering redirects from the old site

--- next.config.js
+++ next.config.js
@@ -13,6 +13,31 @@ const nextConfig = {
         SITE_URL: 'https://www.gansobomb.com'
     },
     basePath: '',
+
+    async redirects() {
+        return [
+            {
+                source: '/tag/:slug/page/:number',
+                destination: '/tag/:slug',
+                permanent: true,
+            },
+            {
+                source: '/category/:slug/page/:number',
+                destination: '/category/:slug',
+                permanent: true,
+            },
+            {
+                source: '/2018/11/04/newcomers-guide-stardom-world',
+                destination: '/posts/2018-11-04-newcomers-guide-stardom-world',
+                permanent: true,
+            },
+            {
+                source: '/2019/03/05/random-review-tetsuya-naito-vs-masato-tanaka',
+                destination: '/posts/2019-03-05-random-review-tetsuya-naito-vs-masato-tanaka',
+                permanent: true,
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
