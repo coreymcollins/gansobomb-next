329f802a56ad69d72e2f207419313fe61c7b4f68
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2024-11-18 20:52:55 -0500

    adds footer links

--- components/FooterText.tsx
+++ components/FooterText.tsx
@@ -7,18 +7,27 @@ export default function HeaderText() {
     
     return (
         <footer>
-            {
-                '/' === pathname && ! searchParams?.has( 'page' ) ?
-                    <>
-                        <p>Ganso Bomb</p>
-                    </>
-                :
-                    <>
-                        <p>Pro wrestling is life.</p>
-                    </>
-            }
+            <div className="footer-top">
+                {
+                    '/' === pathname && ! searchParams?.has( 'page' ) ?
+                        <>
+                            <p>Ganso Bomb</p>
+                        </>
+                    :
+                        <>
+                            <p>Pro wrestling is life.</p>
+                        </>
+                }
 
-            <Link href="mailto:gansobombdotcom@gmail.com?subject=I would like to pay you $5,000 to write something for me">Email me.</Link>
+                <Link href="mailto:gansobombdotcom@gmail.com?subject=I would like to pay you $5,000 to write something for me">Email me.</Link>
+            </div>
+
+            <div className="footer-bottom">
+                <ul className="menu-navigation-list">
+                    <li><Link href="/write-forever">Write Forever Archives</Link></li>
+                    <li><Link href="/rewind">REWIND Archives</Link></li>
+                </ul>
+            </div>
         </footer>
     )
 }
