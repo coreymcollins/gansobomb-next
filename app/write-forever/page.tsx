5f5bde39b557fcb6564d3ce9b6a96fe00ce4668c
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2024-11-18 20:35:25 -0500

    adds standalone pages for write forever and rewind

---
+++ app/write-forever/page.tsx
@@ -0,0 +1,59 @@
+import { getAllPostsByTerm } from '@/components/GetAllPostsByTerm';
+import PaginationControls from '@/components/PaginationControls';
+import PostPreview from '@/components/PostPreview';
+import { getSearchParams } from '@/components/SearchParams';
+
+export default function RewindPage( props: any ) {
+
+	const jsonLd = {
+        '@context': 'https://schema.org/',
+        '@type': 'Blog',
+        '@id': 'https://www.gansobomb.com/write-forever',
+        'mainEntityOfPage': 'https://www.gansobomb.com/write-forever',
+		'url': 'https://www.gansobomb.com/write-forever',
+        'name': 'Ganso Bomb: Write Forever Archives',
+        'description': 'Pro wrestling is life.',
+        'publisher': {
+            '@type': 'Organization',
+            '@id': 'https://www.gansobomb.com',
+            'name': 'Ganso Bomb',
+            'logo': {
+                '@type': 'ImageObject',
+                '@id': 'https://www.gansobomb.com/images/ganso-bomb-fallback.webp',
+                'url': 'https://www.gansobomb.com/images/ganso-bomb-fallback.webp',
+                'width': '960',
+                'height': '640'
+            }
+        },
+    }
+
+    const foundPosts = getAllPostsByTerm( 'category', 'write-forever' );
+    const allSearchParams = getSearchParams(props.searchParams);
+    const pagedPosts = foundPosts.slice(allSearchParams.start, allSearchParams.end);
+    const startIndex = 1000;
+    const rewindPreviews = pagedPosts.map(( post, index ) => (
+        <PostPreview key={post.slug} {...post} index={(index + startIndex).toString()} />
+    ));
+
+	return (
+		<>
+			<script
+				type="application/ld+json"
+				dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+			/>
+			<div className="posts-section">
+				<h2 className="section-heading">Write Forever Archives</h2>
+				<div className="section-grid">
+                    {rewindPreviews}
+				</div>
+			</div>
+            <PaginationControls
+                hasNextPage={allSearchParams.end < foundPosts.length}
+                hasPrevPage={allSearchParams.start > 0}
+                totalPosts={foundPosts.length}
+                postsPerPage={allSearchParams.postsPerPage}
+                query=""
+            />
+		</>
+	)
+}
