e4788bea8c92788faa4a4a4436dd195025f9b4ff
Corey Collins <954724+coreymcollins@users.noreply.github.com>
2025-03-08 23:18:07 -0500

    says fuck it and removes twitter embeds

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,14 +1,5 @@
-'use client'
-
-import React from 'react';
-import { TwitterTweetEmbed } from 'react-twitter-embed';
-
-interface TweetProps {
-    tweetId: string;
+export default function Tweet() {
+    return (
+        <p className="notice">This used to be a Twitter embed. That site is run by nazi dumb fucks now, so I don't really care about embedding their content here anymore.</p>
+    )
 }
-
-const Tweet: React.FC<TweetProps> = ({ tweetId }) => {
-  return <TwitterTweetEmbed tweetId={tweetId} />;
-};
-
-export default Tweet;
